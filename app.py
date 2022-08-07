9dfb1bfe5f2af284c56fdeaacbbb371f15282df0
hitwh33 <87296923+hitwh33@users.noreply.github.com>
2022-08-07 14:41:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def init():
 
 port= int(os.environ.get('PORT',8888))
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=port,debug=True)
+    app.run(host='0.0.0.0',port=port,debug=False)
 
 
 # In[ ]:
